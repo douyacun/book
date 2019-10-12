ed7e9622acd1017f4f1ed33a4dd8452fd918f623
刘宁 <liuning@qutoutiao.net>
2019-10-12 23:44:59 +0800

    deploy

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,4 +2,4 @@
 
 rm -rf .git/
 rsync -avz ./* douyacun:${deploy_dir}
-ssh douyacun "export _DOUYACUN_CONF='${deploy_dir}/configs/douyacun.yml' && /data/www/api.douyacun.com/bin/douyacun deploy -dir ${deploy_dir}"
+ssh douyacun "export _DOUYACUN_CONF='${config_file}' && ${douyacun} deploy -dir ${deploy_dir}"
