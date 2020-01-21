48f6c4a573fdf7ee2f64a3e403422f5f7686d3dd
douyacun <douyacun@gmail.com>
2020-01-21 14:57:09 +0800

    ci

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -avz ./* douyacun:${deploy_dir}
-ssh douyacun "export _DOUYACUN_CONF='${config_file}' && ${douyacun} deploy -dir ${deploy_dir}"
+rsync -avz ./* douyacun:${dir}/book
+ssh douyacun "${dir}/api.douyacun.com/bin/doyacun deploy -config ${dir}/api.douyacun.com/configs/douyacun.yml -dir ${dir}/book/"
