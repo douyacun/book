c15fdea380dc62c410ac9f95241a207104eeaa10
douyacun <douyacun@gmail.com>
2020-01-22 17:16:05 +0800

    .git

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+ls -lah
 rsync -avz ./* douyacun:${dir}/book/
-ssh douyacun "${dir}/api.douyacun.com/bin/douyacun deploy -config ${dir}/api.douyacun.com/configs/douyacun.yml -dir ${dir}/book/"
+ssh douyacun "${dir}/api.douyacun.com/bin/douyacun deploy -conf ${dir}/api.douyacun.com/configs/douyacun.yml -dir ${dir}/book/"
