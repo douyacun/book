6a9f23f553d3123603224758eb2117dffaa94c6f
刘宁 <liuning@qutoutiao.net>
2020-02-18 09:35:04 +0800

    rsync .git

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rsync -avz ./* douyacun:${dir}/book/
+rsync -avz ./.git douyacun:${dir}/book/
 ssh douyacun "${dir}/api.douyacun.com/bin/douyacun deploy -conf ${dir}/api.douyacun.com/configs/douyacun.yml -dir ${dir}/book/"
